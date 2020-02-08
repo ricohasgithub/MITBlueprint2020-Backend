099255f6bef28ba4b87d5ca9c93a09718195b514
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-02-08 13:50:44 -0500

    Completed Part 2

---
+++ part-2/static/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+    $("#submit").click(function () {
+        const message = $("#message").val();
+        $.post('/message', {
+            message: message
+        }).done(function () {
+            document.location.reload();
+        });
+    });
+
+});
